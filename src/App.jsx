37fbb2792df61e6cad2443f7b73a3b2cac19eadd
John Doe <johndoe@example.com>
2025-05-21 22:25:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
-import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
@@ -24,8 +23,6 @@ const App = () => {
       <About setPlayState={setPlayState}/>
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus/>
-        <Title subTitle='Testimonials' title='What Student Says' />
-        <Testimonials/>
         <Title subTitle='Contact Us' title='Get in Touch' />
         <Contact/>
         <Footer/>
